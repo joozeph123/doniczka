c324ef984952ef627523bd4f705292c7349e09fa
Jakub Olejniczak <v3ldrin@o2.pl>
2017-02-12 21:31:56 +0100

    Check once a minute

--- Doniczka.ino
+++ Doniczka.ino
@@ -1,10 +1,3 @@
-/*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
- 
- This example code is in the public domain.
- */
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -14,6 +7,15 @@ void setup() {
   pinMode(2, OUTPUT); 
   pinMode(17, OUTPUT); 
   digitalWrite(17, LOW);    // turn the LED off by making the voltage LOW
+}
+
+void pikaj(int razy) {
+  for(int i = 0; i<razy; i++) {
+    digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(100);
+    digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+    delay(100);
+  }
 }
 
 int poziom = 400;
@@ -27,11 +29,8 @@ void loop() {
   Serial.println(sensorValue);
   
   if (sensorValue < poziom) {
-    // HALASUJEMY
-    digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(100);               // wait for a second
-    digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+    pikaj(3);
   }
   
-  delay(1000);        // delay in between reads for stability
+  delay(60000);
 }
